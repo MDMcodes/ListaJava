4aa27f8fbd3f76ed3ba583f1e43e1e13cb12d651
MDMcodes <matheusdonadelmarques08@gmail.com>
2025-04-09 17:09:40 -0300

    sd
    a

--- ex8.java
+++ ex8.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class ex8 {
     public static void main(String[] args) {
-        // Cria um array de tamanho 10 para armazenar os nomes
-        String[] pessoas = new String[10];
+        // Cria um array de tamanho 5 para armazenar os nomes
+        String[] pessoas = new String[5];
         Scanner scanner = new Scanner(System.in);
-        
         // variavel para verificar nome (true ou false)
         boolean nomeencontrado = false;
 
-        // Solicita ao usuário que insira 10 nomes
-        System.out.println("Digite os 10 nomes das pessoas:");
-        for (int i = 0; i < 10; i++) {
+        // Solicita ao usuário que insira 5 nomes
+        System.out.println("Digite os 5 nomes das pessoas:");
+        for (int i = 0; i < 5; i++) {
             System.out.print("Nome " + (i + 1) + ": ");
             pessoas[i] = scanner.nextLine();
         }
@@ -22,7 +21,7 @@ public class ex8 {
 
        
         // pesquisar se o nome informado ja foi cadastrado no array
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             if (pessoas[i].equalsIgnoreCase(nomeVerificar)) {
             	nomeencontrado = true;
                 break;
